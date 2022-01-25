a0703705165fd1ce757429e57e1cf66b3a575ffe
frtplt <firat_p@hotmail.com>
2022-01-25 19:48:02 +0300

    Initial Commit

---
+++ gitXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitXcode
+//
+//  Created by Firat on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
